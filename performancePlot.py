a183cdd46964397bd051c7ee532dd7d70a48798d
JiayueWU <wjy027@gmail.com>
2018-12-27 15:51:01 -0500

    Add existing

--- performancePlot.py
+++ performancePlot.py
@@ -1,6 +1,6 @@
 import seaborn
 import numpy as np
-import seaborn as sns
+#import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas
 #from loadTrainingData import loadTrainingData
@@ -43,70 +43,3 @@ def computeDistance(distanceMat, assignmentMat):
                 totalDistances -= np.sum(Traj[dup_goals])
                 totalDistances += (count)*max_dist
     return totalDistances
-
-def plotDistance(iterations, optimalDistance, totalDistances):
-    """
-    (INPUT) iterations: (N,) numpy array of iteration numbers
-    (INPUT) optimalDistance: the total distances computed 
-                             using hungarian algorithm 
-    (INPUT) totalDistances: (N,) numpy aaray of totalDistances 
-                            for corresponding iterations
-    """
-
-    x = iterations
-    y = np.ones_like(x) * optimalDistance
-    z = totalDistances 
-    data = {'x':x.astype(int), 
-            'Optimal Distance':y, 
-            'Predicted Distance':z}
-    data = pandas.DataFrame(data)
-    ax = sns.lineplot(data=data,hue='coherence',style='choice')
-    # axes = ax.
-    plt.ylim(0,53)
-    plt.show()
-
-
-
-if __name__ == '__main__':
-    # distanceMat, assignmentMat, _assignment_onehot = loadTrainingData(r'distanceMatricesTest.csv',
-                                                                    #   r'predictedAssignmentMatrices.csv')
-    # distanceMat = np.sum(distanceMat, axis=-1)
-    # totalDistances = computeDistance(distanceMat, assignmentMat)
-    # print(totalDistances)
-    # distanceMatrices = np.loadtxt(r'distanceMatrices.csv',dtype=float)
-    # assignment = np.loadtxt('assignmentMatrices.csv',dtype=int)
-                        
-    x = [i for i in range(10,110,10)]
-    b = [5270784.088698037,
-    5264513.920349974,
- 5267257.39134099,
- 5253386.147280009,
- 5272599.498772029,
- 5262018.43587501,
- 5262365.122317011,
- 5269033.400811051,
- 5255349.365995008,
- 5248668.1135279415]
-    a = 4558860.143774011
-    # c = 4558860.143774011
-    d = [4682115.487961023,
-        4663917.711252073,
-        4663350.176667067,
-        4654199.963472059,
-        4671670.329962055,
-        4667617.328513025,
-        4659446.424542045,
-        4651658.220157067,
-        4656313.687618041,
-        4646471.983179057,
-        4648829.53113705]
-    data = {'x':x, 
-            'Optimal test Distance':a, 
-            'Predicted test Distance':b}
-            # 'Optimal train Distance':c,
-            # 'Predicted train Distance':d}
-    data = pandas.DataFrame(data)
-    ax = sns.lineplot(data=data,
-                      hue='coherence',
-                      style='choice')
-    plt.show()
