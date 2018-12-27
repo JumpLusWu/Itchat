d100808f24b082b29374672ef809ff3eca314830
JiayueWU <wjy027@gmail.com>
2018-12-27 15:09:42 -0500

    Change exiting

--- bidirectionnal_seq2seq_16x16_Train.py
+++ bidirectionnal_seq2seq_16x16_Train.py
@@ -219,27 +219,27 @@ def loading_data(num_robots):
     print("Obtain training data")
     #distanceMatrices = np.loadtxt('distanceMatrices.csv', dtype=float)
     #assignmentMatrices = np.loadtxt('assignmentMatrices.csv', dtype=int)
-    distanceMatrices1 = pandas.read_csv('../../16x16_SeqData/distanceMatrices_Traing_500w.csv',
+    distanceMatrices1 = pandas.read_csv('../../16x16_SeqData/distanceMatrices_train_500w.csv',
                                        header=None,
                                        nrows=5000000,
                                        sep=' ',
                                        dtype='float')
     distanceMatrices1 = distanceMatrices1.values
 
-    distanceMatrices2 = pandas.read_csv('../../16x16_SeqData/distanceMatrices_Traing_300w.csv',
+    distanceMatrices2 = pandas.read_csv('../../16x16_SeqData/distanceMatrices_train_300w.csv',
                                        header=None,
                                        nrows=3000000,
                                        sep=' ',
                                        dtype='float')
     distanceMatrices2 = distanceMatrices2.values
     distanceMatrices = np.concatenate((distanceMatrices1,distanceMatrices2))
-    assignmentMatrices1 = pandas.read_csv('../../16x16_SeqData/assignmentMatrices_Traing_500w.csv',
+    assignmentMatrices1 = pandas.read_csv('../../16x16_SeqData/assignmentMatrices_train_500w.csv',
                                        header=None,
                                        nrows=5000000,
                                        sep=' ',
                                        dtype='float')
     assignmentMatrices1 = assignmentMatrices1.values
-    assignmentMatrices2 = pandas.read_csv('../../16x16_SeqData/assignmentMatrices_Traing_300w.csv',
+    assignmentMatrices2 = pandas.read_csv('../../16x16_SeqData/assignmentMatrices_train_300w.csv',
                                        header=None,
                                        nrows=3000000,
                                        sep=' ',
