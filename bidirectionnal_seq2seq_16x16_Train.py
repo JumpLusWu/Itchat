10767c14a3e4f3130d21e47927459aae27c01c72
JiayueWU <wjy027@gmail.com>
2018-12-27 17:01:35 -0500

    Add

--- bidirectionnal_seq2seq_16x16_Train.py
+++ bidirectionnal_seq2seq_16x16_Train.py
@@ -222,7 +222,7 @@ def loading_data(num_robots):
     #assignmentMatrices = np.loadtxt('assignmentMatrices.csv', dtype=int)
     distanceMatrices1 = pandas.read_csv('../../16x16_SeqData/distanceMatrices_train_500w.csv',
                                        header=None,
-                                       nrows=500000,
+                                       nrows=1000000,
                                        sep=' ',
                                        dtype='float')
     distanceMatrices1 = distanceMatrices1.values
@@ -236,7 +236,7 @@ def loading_data(num_robots):
     distanceMatrices = np.concatenate((distanceMatrices1,distanceMatrices2))
     assignmentMatrices1 = pandas.read_csv('../../16x16_SeqData/assignmentMatrices_train_500w.csv',
                                        header=None,
-                                       nrows=500000,
+                                       nrows=1000000,
                                        sep=' ',
                                        dtype='float')
     assignmentMatrices1 = assignmentMatrices1.values
